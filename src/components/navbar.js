3c363e188684b732204ea1c546be42b12adba360
Light3039 <holypaladin47@gmail.com>
2022-05-05 17:51:17 +0430

    Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,45 @@
 import React from "react"
 import { Link } from "gatsby"
+import { nav, navList, navItemBtn, navItemSeparator } from '../styles/navbar.module.css'
 
 const Navbar = () => {
     return (
-        <nav>
-            <h1> Light </h1>
-            <div className="links">
-                <Link to="/"> Home </Link>
-                <Link to="/about"> About </Link>
-            </div>
+        <nav className={nav}>
+            <ul className={navList}>
+
+                <Link to="/" className={navItemBtn}>
+                    <li >
+                        H O M E
+                    </li>
+                </Link>
+                <li className={navItemSeparator}>
+                </li>
+
+                <Link to="/" className={navItemBtn}>
+                    <li >
+                        C O D E
+                    </li>
+                </Link>
+
+                <Link to="/" className={navItemBtn}>
+                    <li >
+                        A R T
+                    </li>
+                </Link>
+                <Link to="/" className={navItemBtn}>
+                    <li className>
+                        M U S I C
+                    </li>
+                </Link>
+                <li className={navItemSeparator}>
+                </li>
+
+                <Link to="/" className={navItemBtn}>
+                    <li >
+                        A B O U T
+                    </li>
+                </Link>
+            </ul>
         </nav>
     )
 }
