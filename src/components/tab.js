471b66e90c9cb3c7da2522dcc23803f3c4aa6dc9
Light3039 <holypaladin47@gmail.com>
2022-05-13 15:37:05 +0430

    Develop Tab Component

--- src/components/tab.js
+++ src/components/tab.js
@@ -1,24 +1,41 @@
 import * as React from "react"
 import * as style from '../styles/tab.module.scss'
 
+function ShowTab(tabContainerId, tabIndex) {
+    console.log(tabContainerId);
+    var tabContainer = document.getElementById(tabContainerId);
+    var tabContents = tabContainer.querySelectorAll(`.${style.tabContent}`);
+
+    console.log(tabIndex);
+    console.log(tabContents.length);
+    console.log('---');
+
+    tabContents.forEach(function(node) {
+        node.style.display = "none";
+    });
+
+    tabContents[tabIndex].style.display = "block";
+}
+
 export default function Tab(props) {
+
     const tabs = props.tabs;
     const contents = props.contents;
+    const id = Math.random();
 
-    const tabButtons = tabs.map((tab) =>
-        <button className={style.tabButton}> {tab} </button>
+    const tabButtons = tabs.map((tab, index) =>
+        <button className={style.tabButton} onClick={(event) => ShowTab(id, index)}> {tab} </ button >
 
     );
 
     const tabContent = contents.map((content) =>
-        <div className={style.tabContent}>
+        <div className={style.tabContent} on>
             {content}
         </div >
     );
 
-
     return (
-        <div className={style.tabContainer}>
+        <div className={style.tabContainer} id={id}>
 
             <div className={style.buttonContainer}>
                 {tabButtons}
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,16 +4,41 @@ import Social from '../components/social'
 import Tab from '../components/tab'
 import * as style from '../styles/about.module.scss'
 
+
 export default function About() {
 
     const Specs = [
-        <p> Spec1 </p>,
-        <p> Spec2 </p>
+        <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>,
+        <p>
+            AWrkljWAE:Tkljawlok tj oalwjtboawijoaWejbapowrtJ WATpboaiwtpaowijrPOAWIjer
+            oawejtbawpeotjaweptboahweptboahwetpoaiwhetbopbaiwetbawet
+            awetba;weoltjbawpetoibjaweptoijbawetpo;bijawet
+            awetbaoweitjbaowiethjbawet
+        </p>,
+
     ];
 
     const SpecsTabs = [
-        <p> Btn1 </p>,
-        <p> Btn2 </p>
+        <h1> Lina </h1>,
+        <h1> Tresdin </h1>,
+    ]
+
+    const Resoureces = [
+        <p> Programming resources </p>,
+        <p> Art resources </p>,
+        <p> Music resources </p>,
+    ]
+
+    const ResourecesTabs = [
+        <h1> Programming </h1>,
+        <h1> Art </h1>,
+        <h1> Music </h1>,
+
     ]
 
     return (
@@ -45,17 +70,11 @@ export default function About() {
                     </div>
 
                     <div className={style.specs}>
+                        <h1> SPECS </h1>
                         <Tab
                             tabs={SpecsTabs}
                             contents={Specs}
                         />
-                        <h1> SPECS </h1>
-                        <p>
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                            no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                        </p>
                     </div>
 
                     <div className={style.hseparator}>
@@ -63,22 +82,10 @@ export default function About() {
 
                     <div className={style.resources}>
                         <h1> RESOURCES </h1>
-                        <p>
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                            no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                            no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                            no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                        </p>
+                        <Tab
+                            tabs={ResourecesTabs}
+                            contents={Resoureces}
+                        />
                     </div>
 
                 </div>
