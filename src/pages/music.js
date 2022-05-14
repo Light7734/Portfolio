509bc7f81dd9d18adb0cb94a5944f70328ee606e
Light3039 <holypaladin47@gmail.com>
2022-05-15 03:55:26 +0430

    Develop Music Page

--- src/pages/music.js
+++ src/pages/music.js
@@ -7,10 +7,18 @@ export default function Music() {
     return (
         <Layout title="Music >_<"
             left={
-                <h1> PROJECTS </h1>
+                <div>
+                    <h1>
+                        PROJECTS
+                    </h1>
+                    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1263030598&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div className={style.soundCloud}></div>
+                </div>
             }
             right={
-                <h1> ARTICLES </h1>
+                <div>
+                    <h1> ARTICLES </h1>
+                    <h4> No articles yet... ^^ </h4>
+                </div>
             }
         >
         </Layout >
