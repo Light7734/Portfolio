31c3e47575799e15ae9450bf7e847b9e031193c8
Light3039 <holypaladin47@gmail.com>
2022-05-30 16:54:26 +0430

    Develop: Art Page, ArtFrame Component

--- src/components/artFrame.tsx
+++ src/components/artFrame.tsx
@@ -1,18 +1,23 @@
 import * as React from "react"
-import * as style from '../styles/codeFrame.module.scss'
 
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { GatsbyImage, GatsbyImageProps } from "gatsby-plugin-image"
+
+import * as style from '../styles/artFrame.module.scss'
 
 interface ArtFramepRops {
+
+    image: GatsbyImageProps["image"];
+    alt: string;
 }
 
 const ArtFrame: React.FC<ArtFramepRops> = (props) => {
 
-
     return (
         <div className={style.container}>
-
+            <GatsbyImage className={style.image} image={props.image} alt={props.alt} />
         </div>
     )
 
 }
+
+export default ArtFrame;
--- src/pages/art.tsx
+++ src/pages/art.tsx
@@ -1,17 +1,51 @@
 import * as React from "react"
 import Layout from '../components/layout'
 
+import { graphql } from "gatsby"
+
+import ArtFrame from "../components/artFrame"
+
 import * as style from '../styles/art.module.scss'
 import * as styleCommon from "../styles/common.module.scss"
 
-const Art: React.FC = () => {
+const Art: React.FC = (query) => {
+
+    const pixelArts: any[] = query.data.pixelArts.edges;
+    const blenderArts: any[] = query.data.blenderArts.edges;
+
+    const pixelArtFrames = pixelArts.map((art: any, index: number) =>
+        <ArtFrame
+            image={art.node.childImageSharp.gatsbyImageData}
+            alt="Test"
+        />
+    );
+
+    const blenderArtFrames = blenderArts.map((art: any, index: number) =>
+        <ArtFrame
+            image={art.node.childImageSharp.gatsbyImageData}
+            alt="Test"
+        />
+    );
 
     return (
         <Layout
             title="Art >_<"
             left={
                 <div>
-                    <h1> PROJECTS </h1>
+                    <h1> Projects</h1>
+                    <div className={styleCommon.hseparator} />
+
+                    <h2>PixelArts</h2>
+                    <div className={styleCommon.hseparator} />
+                    {pixelArtFrames}
+                    {pixelArtFrames}
+                    {pixelArtFrames}
+                    {pixelArtFrames}
+
+                    <h1> 3DArt</h1>
+                    <div className={styleCommon.hseparator} />
+
+                    <h1> VectorArt </h1>
                     <div className={styleCommon.hseparator} />
                 </div>
             }
@@ -28,3 +62,30 @@ const Art: React.FC = () => {
 }
 
 export default Art;
+
+export const query = graphql`
+query ArtQuery {
+  pixelArts: allFile(
+    filter: {relativeDirectory: {in: "pixel"}, sourceInstanceName: {eq: "arts"}}
+  ) {
+    edges {
+      node {
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+    }
+  }
+  blenderArts: allFile(
+    filter: {relativeDirectory: {in: "3d"}, sourceInstanceName: {eq: "arts"}}
+  ) {
+    edges {
+      node {
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+    }
+  }
+}
+`
