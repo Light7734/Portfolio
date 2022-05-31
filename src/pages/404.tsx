d8082baa6026b9faf98f5b0c3add825cc5bea317
Light3039 <holypaladin47@gmail.com>
2022-05-31 15:13:24 +0430

    Develop: 404 Page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,8 +8,18 @@ const NotFound: React.FC = () => {
     return (
         <Layout
             title="404 >_<"
-            left={<h1> 404 </h1>}
-            right={<h1> uwu </h1>}
+            left={
+                <div>
+                    <h1> 404 </h1>
+                    <h2> Requested page not found! </h2>
+                </div>
+            }
+            right={
+                <div>
+                    <h2> We are no strangers to love </h2>
+                    <h2> You know the rules and so do I ... </h2>
+                </div>
+            }
         />
     )
 
