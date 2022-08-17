2308d2ada77cc835b6e69483ae27c04d54c44166
Light7734 <mail@mohammad-hm.com>
2022-08-17 19:25:41 +0430

    fix: socials and tools of the trade

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -164,10 +164,10 @@ const About: React.FC = () => {
                     <div className={cstyle.hseparator} />
                     <div className={style.socials}>
                         <Social caption={"Github"} link={"https://github.com/light7734"} icon={"/icons/github.svg"} description="All my code projects" />
-                        <Social caption={"Email"} link={"mailto:mail@mohammad-hm.com"} icon={"/icons/mail.svg"} description="mail@mohammad-hm.com" />
+                        <Social caption={"Email"} link={"mailto:mail@light7734-hm.com"} icon={"/icons/mail.svg"} description="mail@light7734-hm.com" />
                         <Social caption={"Twitter"} link={"https://twitter.com/Light7734"} icon={"/icons/twitter.svg"} description="Daily art posts & retweets" />
                         <Social caption={"Discord"} link={"https://discordapp.com/users/928576662352822353"} icon={"/icons/discord.svg"} description="Chat with me @Light7734#4652" />
-                        <Social caption={"Youtube"} link={"https://www.youtube.com/channel/UC6rAJG66oNHIeb5-50RhdDw"} icon={"/icons/youtube.svg"} description="Devlogs (soon, hopefully)" />
+                        <Social caption={"Youtube"} link={"https://www.youtube.com/channel/UCIVFJTiSJsUZYY4XAWQj2jQ"} icon={"/icons/youtube.svg"} description="Devlogs (soon, hopefully)" />
                         <Social caption={"Pinterest"} link={"https://www.pinterest.com/Light7734_/_saved/"} icon={"/icons/pinterest.svg"} description="Art references & inspirations" />
                         <Social caption={"Reddit"} link={"https://www.reddit.com/user/Light7734"} icon={"/icons/reddit.svg"} description="Idk what to do with this" />
                         <Social caption={"Soundcloud"} link={"https://soundcloud.com/user-499649814"} icon={"/icons/soundcloud.svg"} description="All my music" />
@@ -182,10 +182,8 @@ const About: React.FC = () => {
                         <Social caption={"Artirx Linux"} link={"https://artixlinux.org/"} icon={"/icons/artixbtw.svg"} description="Lina's operating system" />
                         <Social caption={"Gentoo Linux"} link={"https://www.gentoo.org/"} icon={"/icons/gentoo.svg"} description="Tresdin's operating system" />
                         <Social caption={"Neovim"} link={"https://neovim.io/"} icon={"/icons/nvim.svg"} description="IDE and Text-editor" />
-                        <Social caption={"Rider"} link={"https://www.jetbrains.com/lp/rider-unreal/"} icon={"/icons/rider.svg"} description="IDE for UnrealEngine" />
                         <Social caption={"Blender"} link={"https://www.blender.org/"} icon={"/icons/blender-3d.svg"} description="3D-art creation tool" />
                         <Social caption={"Aseprite"} link={"https://www.aseprite.org/"} icon={"/icons/aseprite.svg"} description="Pixel-art creation tool" />
-                        <Social caption={"Unreal Engine"} link={"https://www.unrealengine.com/en-US/"} icon={"/icons/ue.svg"} description="3D game engine" />
                         <Social caption={"Light Engine"} link={"https://github.com/light7734/light"} icon={"/icons/light.svg"} description="My own game engine ^^" />
                         <Social caption={"C++"} link={"https://en.cppreference.com/w/"} icon={"/icons/c++.svg"} description="Favourite programming language" />
                         <Social caption={"Rust"} link={"https://www.rust-lang.org/"} icon={"/icons/rust.svg"} description="Learning to make a renderer with it" />
